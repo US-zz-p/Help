736c6f5ecc66d9596db00e7e0c91fcb238320438
John Robson <John.Robson@USP.br>
2016-08-19 00:31:52 -0400

    updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, make_response, request, redirect, url_for
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
@@ -7,9 +7,16 @@ from flask_wtf import CsrfProtect
 csrf = CsrfProtect()
 csrf.init_app(app)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  #session.clear()
+  #session['session1'] = 'value of session1'
+  resp = make_response(render_template('maintenance.html'))
+  #resp.set_cookie('cookie1', 'value of cookie1')
+  #resp.set_cookie('sessionID', '', expires=0)
+  return resp
+
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5001, use_reloader=True, threaded=True)
+  app.run(debug=True, host='0.0.0.0', port=5001)

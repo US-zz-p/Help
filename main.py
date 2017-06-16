5c11a7b51cec2c1d1f3c813de2af60bf4c713dce
John Robson <John.Robson@USP.br>
2017-06-16 02:05:00 -0400

    updates

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# https://github.com/USP/Help
-# https://usp.herokuapp.com/
-
-# https://dashboard.heroku.com/apps/usp/activity
-# https://dashboard.heroku.com/apps/usp/logs
-
 from flask import Flask, render_template, make_response  # request, redirect, session, url_for
 from flask_wtf import CSRFProtect
 
@@ -50,4 +44,3 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
 
 # $ python main.py
-# http://pythonhow.com/building-a-website-with-python-flask/
